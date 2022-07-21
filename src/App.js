e34bf1d2d8895ce6ca5e3a9636642317e2192fcd
Ximilis <92741358+maxhomsi@users.noreply.github.com>
2022-07-20 22:20:32 -0500

    jul19

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+// Functional Component e Class-Based Component
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Max
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isClicked: false,
+      inputValue: "",
+      listOfTodos: [],
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ inputValue: event.target.value });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault(); //tem q colocar isso para nao sumir o Data
+    this.setState({
+      listOfTodos: [...this.state.listOfTodos, this.state.inputValue],
+    });
+    this.setState({ inputValue: "" });
+  };
+
+  handleClick = (index) => {
+    let objectCopy = [...this.state.listOfTodos];
+    objectCopy.splice(index, 1);
+    this.setState({ listOfTodos: [...objectCopy] });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.inputValue}
+            onChange={this.handleChange}
+          ></input>
+          <button type="submit">Submit here</button>
+        </form>
+        <ol>
+          {this.state.listOfTodos.map((todo, index) => {
+            return (
+              <div>
+                <li key={index}>{todo} </li>
+                <button onClick={() => this.handleClick(index)}>Delete</button>
+              </div>
+            );
+          })}
+        </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
